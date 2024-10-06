d7f88daff68e8990d8917050c7ca3d32629d1ab1
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-10-06 19:38:13 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import { assets } from '../assets/assets'
 const Contact = () => {
   return (
     <div className='text-lg'>
-      <div className='text-center text-3xl pt-10 border-t'>
+      <div className='text-center text-xl sm:text-3xl pt-10 border-t'>
         <Title text1={'CONTACT'} text2={'US'}/>
       </div>
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
