3813b0a0dab7ed1b1b86b9aadba10397b0c5d342
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-10-06 19:34:39 +0530

    Update LatestCollection.jsx

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ const LatestCollection = () => {
 
   return (
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
+        <div className='text-center py-8 text-xl sm:text-3xl'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-sm  md:text-base text-gray-600'>
             Check out the latest collection of tech products from our store.
