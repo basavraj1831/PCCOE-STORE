e3cf48dfdf7ad862282ac06bb31d60b7dcd2901b
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-10-06 22:35:37 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       <div className='text-center text-xl sm:text-3xl pt-10 border-t'>
         <Title text1={'CONTACT'} text2={'US'}/>
       </div>
-      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-16'>
         <img src={assets.contact_img} className='w-full md:max-w-[480px]' alt=''/>
         <div className='flex flex-col gap-6 justify-center items-start'>
           <p className='text-xl font-semibold text-gray-600'>Our Store</p>
