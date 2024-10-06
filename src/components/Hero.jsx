ff6fc4809af506aa107c10d12bbdd37951383daa
basavraj1831 <144144279+basavraj1831@users.noreply.github.com>
2024-10-06 19:52:30 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Hero = () => {
   return (
     <div className='flex flex-col sm:flex-row border border-gray-400 h-[560px]'>
-        <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
+        <div className='w-full sm:w-1/2 flex items-center justify-center py-[36.5px] sm:py-0'>
             <div className='text-[#414141]'>
                 <div className='flex items-center gap-2'>
                     {/* <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p> */}
